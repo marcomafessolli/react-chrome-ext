43bcd10aac64ffd2de199bd9aed6f69cd3b4765f
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2022-12-31 04:43:39 -0300

    feat(css): adds tailwind

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -2,10 +2,10 @@ import reactLogo from "./images/react.svg";
 
 function Popup() {
   return (
-    <>
-      <h3>React Chrome Extension Starter</h3>
-      <img src={chrome.runtime.getURL(reactLogo)} alt="" />
-    </>
+    <div className="w-80 h-80 items-center justify-center flex flex-col bg-gray-500 text-white text-center">
+      <h1 className="text-3xl font-bold">React Chrome Extension Starter</h1>
+      <img src={chrome.runtime.getURL(reactLogo)} alt="" className="w-6/12 mt-4"/>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import './main.css';
 import Popup from "./Popup";
 
 if (!chrome?.runtime) {
