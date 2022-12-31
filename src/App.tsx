56e6755b4c58bd053c3a437edf803e6677946aaa
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2022-12-30 22:19:28 -0300

    chore(core): inits app and updates dev configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './images/react.svg'
+
+function App() {
+  return (
+    <div>
+        Hello sir!
+    </div>
+  )
+}
+
+export default App
